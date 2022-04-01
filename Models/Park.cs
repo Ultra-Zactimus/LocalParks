f98a21f6ccf8e160e0031d89bba305d937a9a3b5
Ultra Zacky <ultrazacky@gmail.com>
2022-04-01 10:41:12 -0700

    add GetParks to Park Model

--- Models/Park.cs
+++ Models/Park.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LocalPark.Models
 {
@@ -11,5 +12,20 @@ namespace LocalPark.Models
     public string City { get; set; }
     public string Trails { get; set; }
     public string Notes {  get; set; }
+
+    public static List<Park> GetParks()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert
+        .DeserializeObject<JObject>
+        (result);
+      List<Park> parkList = JsonConvert
+        .DeserializeObject<List<Park>>
+        (jsonResponse.ToString());
+      
+      return parkList;
+    }
   }
 }
