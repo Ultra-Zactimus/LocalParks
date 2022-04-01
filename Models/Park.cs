004164e2e255c92a148ea53b45389da5ff66e0a1
Ultra Zacky <ultrazacky@gmail.com>
2022-04-01 08:39:43 -0700

    add Park Class structure

---
+++ Models/Park.cs
@@ -0,0 +1,11 @@
+namespace LocalPark
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Trails { get; set; }
+    public string Notes {  get; set; }
+  }
+}
