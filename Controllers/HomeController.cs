5b8a47b63335e0e4520cb6773905fa7ddaa2a8b6
Ultra Zacky <ultrazacky@gmail.com>
2022-04-01 09:52:29 -0700

    add Index to Home Controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LocalPark.Models;
+
+namespace LocalPark.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return RedirectToAction("Index", "Parks");
+    }
+  }
+}
