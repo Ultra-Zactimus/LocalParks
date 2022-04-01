38c09cc90b9e1bb48dbfb782db5d004786ca96cb
Ultra Zacky <ultrazacky@gmail.com>
2022-04-01 09:35:45 -0700

    add Delete to ParksController

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -39,5 +39,12 @@ namespace LocalPark.Models
       var response = await client.ExecuteTaskAsync(request);
     }
 
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"parks/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecutePostTaskAsync(request);
+    }
   }
 }
