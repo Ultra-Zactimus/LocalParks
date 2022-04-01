86c2469459de3b155c3f6206ede4c291248bd452
Ultra Zacky <ultrazacky@gmail.com>
2022-04-01 11:19:23 -0700

    add more refactoring

--- Startup.cs
+++ Startup.cs
@@ -27,32 +27,45 @@ namespace LocalPark
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocalPark", Version = "v1" });
-            });
+            services
+                .AddControllers();
+            services
+                .AddSwaggerGen(c =>
+                {
+                    c
+                        .SwaggerDoc("v1", new OpenApiInfo { Title = "LocalPark", Version = "v1" });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env
+                .IsDevelopment())
+                {
+                    app
+                        .UseDeveloperExceptionPage();
+                    app
+                        .UseSwagger();
+                    app
+                        .UseSwaggerUI(c => c
+                            .SwaggerEndpoint("/swagger/v1/swagger.json", "LocalPark v1"));
+                }
+
+            app
+                .UseHttpsRedirection();
+
+            app
+                .UseRouting();
+
+            app
+                .UseAuthorization();
+
+            app
+                .UseEndpoints(endpoints =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalPark v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+                endpoints
+                    .MapControllers();
             });
         }
     }
